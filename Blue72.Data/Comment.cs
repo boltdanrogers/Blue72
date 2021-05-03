27bfbc364239066593a4ba63eee903975ab899cd
Daniel Rogers <boldanrogers@gmail.com>
2021-05-03 14:49:38 -0400

    coded out like and reply, now app will register new users

--- Blue72.Data/Comment.cs
+++ Blue72.Data/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace Blue72.Data
         public string Text { get; set; }
         [Required]
         public Guid AuthorId { get; set; }
-        
+        [ForeignKey(nameof(Post))]
         public int PostId { get; set; }
         public virtual Post Post { get; set; }
 
